052f086f5d4aa84cda3045ba20f7c46cb8ae14c4
Bitz26 <abidramdan26@gmail.com>
2025-01-17 08:24:46 +0800

    Update content.js

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -1,8 +1,8 @@
 import { InlineCode } from "@/once-ui/components";
 
 const person = {
-    firstName: 'Selene',
-    lastName:  'Yu',
+    firstName: 'Wowowo',
+    lastName:  'hhah',
     get name() {
         return `${this.firstName} ${this.lastName}`;
     },
